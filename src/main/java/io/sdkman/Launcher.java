885f198a7349f5ef1b52acf52b21d083ecf0b7ee
Huang Xiao <youngledo@qq.com>
2025-11-19 22:49:32 +0800

    feature: Support native image.

---
+++ src/main/java/io/sdkman/Launcher.java
@@ -0,0 +1,9 @@
+package io.sdkman;
+
+import javafx.application.Application;
+
+public class Launcher {
+    static void main(String[] args) {
+        Application.launch(SdkmanApplication.class, args);
+    }
+}
